f0745b2f3f58741a564dcab6ecbc35eb3099d424
Italo Carlo <italocarlo@nti.ufal.br>
2018-12-03 22:39:37 -0300

    Endpoint /pedidos/{i} Entregue

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -10,6 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -21,9 +22,11 @@ public abstract class Pagamento  implements Serializable {
 	private Integer id;
 	private Integer estado;
 
+	
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
+	@JsonBackReference
 	private Pedido pedido;
 	
 	public Pagamento() {
