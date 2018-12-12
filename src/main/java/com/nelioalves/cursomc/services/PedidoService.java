c83ad91a746d21a488cf5f25252490664f29b1de
Italo Carlo <italocarlo@nti.ufal.br>
2018-12-11 21:28:14 -0300

    Email html

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -73,7 +73,7 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		itemPedidoRepository.saveAll(obj.getItens());
 		
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		//System.out.println(obj.toString());
 		
 		
